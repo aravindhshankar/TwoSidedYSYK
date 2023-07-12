7ce389f0b62186e3a8852d5936d1bc341658aae7
aravindhshankar <aravindhswaminathan@gmail.com>
2023-07-12 18:23:26 +0200

    eod commit
    possibly something funky going on in bosonic fourier trransform

--- ConformalAnalytical.py
+++ ConformalAnalytical.py
@@ -9,3 +9,16 @@ def GconfImag(omega,g,beta):
     delta = 0.420374134464041
     
     return 1/((1j*omega) * (1 + (c1*(np.abs((g**2)/omega)**(2*delta)))))
+
+def DconfImag(nu,g,beta):
+    ''' 
+    So far we will implement only the kappa=1 result eq.16 of esterlis-schmalian
+    nu is the grid of fermionic matsubara frequencies
+    '''
+    T = 1.0/beta
+    c2 = 0.561228
+    c3 = 0.709618
+    delta = 0.420374134464041
+    omegar2 = c2 * (T/(g**2))**(4*delta - 1)
+    
+    return 1.0/(nu**2 + omegar2 + c3*(np.abs(nu/(g**2)))**(4*delta - 1))
