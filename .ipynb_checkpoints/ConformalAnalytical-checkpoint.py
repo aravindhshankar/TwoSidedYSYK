f9183a9fd54691fb4d4b8bf407a02f63e2513601
aravindhshankar <aravindhswaminathan@gmail.com>
2023-07-17 13:11:31 +0200

    Notebooks to check the bosonic FFT
    anomalous cosh in DtauAnalytic

--- .ipynb_checkpoints/ConformalAnalytical-checkpoint.py
+++ .ipynb_checkpoints/ConformalAnalytical-checkpoint.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def GconfImag(omega,g,beta):
     ''' 
+    Arguments omega,g,beta
     So far we will implement only the kappa=1 result eq.16 of esterlis-schmalian
     omega is the grid of fermionic matsubara frequencies
     '''
@@ -12,6 +13,7 @@ def GconfImag(omega,g,beta):
 
 def DconfImag(nu,g,beta):
     ''' 
+    Arguments: nu,g,beta
     So far we will implement only the kappa=1 result eq.16 of esterlis-schmalian
     nu is the grid of fermionic matsubara frequencies
     '''
@@ -22,3 +24,14 @@ def DconfImag(nu,g,beta):
     omegar2 = c2 * (T/(g**2))**(4*delta - 1)
     
     return 1.0/(nu**2 + omegar2 + c3*(np.abs(nu/(g**2)))**(4*delta - 1))
+
+def DfreeImagtau(tau,r,beta):
+    '''
+    Arguments: tau,r,beta
+    Obtained by contour integration of Dfreeomega
+    '''
+    m = np.sqrt(r)
+    pref = 1./(2*m)
+    num = (tau-(beta/2))*m
+    den = beta*m/2
+    return pref * np.cosh(num)/np.sinh(den)
