d512a6672e5c21b56c569d0418dc11d9ee50312e
aravindhshankar <aravindhswaminathan@gmail.com>
2023-12-19 17:06:11 +0100

    commit before warsaw

--- ClusterScript/Plotting_cWH.py
+++ ClusterScript/Plotting_cWH.py
@@ -14,7 +14,7 @@ import testingscripts
 from h5_handler import *
 
 path_to_outfile = './Outputs'
-outfile = 'testcSYK.h5'
+outfile = 'cSYK_WH_2165159.h5'
 savepath = os.path.join(path_to_outfile, outfile)
 
 if not os.path.exists(savepath):
@@ -28,7 +28,7 @@ dw = np.pi/data['T']
 temp = 1./data['beta']
 
 titlestring = 'beta = ' + str(data['beta']) + ' mu = ' + str(data['mu'])
-titlestring += ' Log2M = ' + str(np.log2(data['M']))
+titlestring += ' Log2M = ' + str(np.log2(data['M'])) + 'kappa = ' + str(data['kappa'])
 
 
 fig, (ax1,ax2) = plt.subplots(1,2)
@@ -40,11 +40,11 @@ ax1.plot(data['omega'], data['rhoGD'],'.-')
 ax1.set_xlabel('omega')
 ax1.set_ylabel('rhoGD')
 #ax1.set_xlim(-20,60)
-ax1.set_xlim(-0.1,0.1)
+ax1.set_xlim(-0.5,0.5)
 
 ax2.plot(data['omega'], data['rhoGOD'],'.-')
 #ax2.set_xlim(-20,60)
-ax2.set_xlim(-0.1,0.1)
+ax2.set_xlim(-0.5,0.5)
 ax2.set_xlabel('omega')
 ax2.set_ylabel('rhoGOD')
 
