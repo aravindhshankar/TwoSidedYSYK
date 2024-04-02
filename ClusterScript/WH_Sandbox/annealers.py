3c7b7eebf5c1720cc2e8858e5aadf3eb05afae46
aravindhshankar <aravindhswaminathan@gmail.com>
2024-04-02 22:11:07 +0200

    added temperature annealer

--- ClusterScript/WH_Sandbox/annealers.py
+++ ClusterScript/WH_Sandbox/annealers.py
@@ -19,133 +19,204 @@ from ConformalAnalytical import *
 from free_energy import free_energy_rolling_YSYKWH 
 #import time
 
-err = 1e-6
+#err = 1e-8
+err = 1e-5
 ITERMAX = 5000
 
 
-def anneal_temp(GFtaus,Nbig,betas,g,r,mu,lamb,J,kappa,DUMP=False,path_to_dump=None,calcfe=True,verbose=True):
-	if verbose: print("############ Started : target beta = , ", target_beta, " #############")
+def anneal_temp(target_beta,GFtaus,Nbig,beta_start,g,r,mu,lamb,J,kappa,DUMP=False,path_to_dump=None,calcfe=False,verbose=True):
+	if verbose: 
+		print("############ Started : target beta = , ", target_beta, " #############")
 	GDtau,GODtau,DDtau,DODtau = GFtaus
-	beta_start, target_beta = betas
 	beta = beta_start
 
 	fe_list = [] #could be slow - try prealloacating for speed later
-
+	fe = 0.0
 
 	assert len(GDtau) == Nbig, 'Improperly loaded starting guess'
 
-	while(beta < target_beta):
-	    itern = 0
-	    diff = err*1.1
-	    diffG = 1.
-	    diffD = 1.
-	    x = 0.01
-	    beta_step = 1 if (beta>=500) else 1
+	while(beta <= target_beta):
+		itern = 0
+		diff = err*1.1
+		diffG = 1.
+		diffD = 1.
+		x = 0.01
+		beta_step = 1 if (beta>=500) else 1
 
-	    omega = ImagGridMaker(Nbig,beta,'fermion')
-	    nu = ImagGridMaker(Nbig,beta,'boson')
-	    tau = ImagGridMaker(Nbig,beta,'tau')
-	    
-	    diff = 1.
-	    iterni=0
-	    while(diff>err and itern < ITERMAX):
-	        itern+=1
-	        iterni += 1 
+		omega = ImagGridMaker(Nbig,beta,'fermion')
+		nu = ImagGridMaker(Nbig,beta,'boson')
+		tau = ImagGridMaker(Nbig,beta,'tau')
+		
+		diff = 1.
+		iterni=0
+		while(diff>err and itern < ITERMAX):
+			itern+=1
+			iterni += 1 
 
-	        oldGDtau, oldGODtau = 1.0*GDtau, 1.0*GODtau
-	        oldDDtau, oldDODtau = 1.0*DDtau, 1.0*DODtau
-	        
-	        if iterni == 1:
-	            oldGDomega,oldGODomega = Time2FreqF(oldGDtau,Nbig,beta),Time2FreqF(oldGODtau,Nbig,beta)
-	            oldDDomega, oldDODomega = Time2FreqB(oldDDtau,Nbig,beta),Time2FreqB(oldDODtau,Nbig,beta)
-	        else:
-	            oldGDomega, oldGODomega = 1.0*GDomega, 1.0*GODomega
-	            oldDDomega, oldDODomega = 1.0*DDomega, 1.0*DODomega
-	        
-	        SigmaDtau = 1.0 * kappa * (g**2) * DDtau * GDtau
-	        SigmaODtau = 1.0 * kappa * (g**2) * DODtau * GODtau
-	        PiDtau = 2.0 * g**2 * GDtau * GDtau[::-1] #KMS G(-tau) = -G(beta-tau)
-	        PiODtau = 2.0 * g**2 * GODtau * GODtau[::-1] #KMS G(-tau) = -G(beta-tau)
-	        
-	        SigmaDomega, SigmaODomega = Time2FreqF(SigmaDtau,Nbig,beta),Time2FreqF(SigmaODtau,Nbig,beta)
-	        PiDomega, PiODomega =  Time2FreqB(PiDtau,Nbig,beta), Time2FreqB(PiODtau,Nbig,beta)
-	        
-	        detG = (1j*omega+mu-SigmaDomega)**2 - (lamb - SigmaODomega)**2
-	        detD = (nu**2 + r - PiDomega)**2 - (J - PiODomega)**2
-	        GDomega = x*((1j*omega + mu - SigmaDomega)/(detG)) + (1-x)*oldGDomega
-	        GODomega = x*(-1.*(lamb- SigmaODomega)/(detG)) + (1-x)*oldGODomega
-	        DDomega = x*((nu**2 + r - PiDomega)/(detD)) + (1-x)*oldDDomega
-	        DODomega = x*(-1.*(J- PiODomega)/(detD)) + (1-x)*oldDODomega
+			oldGDtau, oldGODtau = 1.0*GDtau, 1.0*GODtau
+			oldDDtau, oldDODtau = 1.0*DDtau, 1.0*DODtau
+			
+			if iterni == 1:
+				oldGDomega,oldGODomega = Time2FreqF(oldGDtau,Nbig,beta),Time2FreqF(oldGODtau,Nbig,beta)
+				oldDDomega, oldDODomega = Time2FreqB(oldDDtau,Nbig,beta),Time2FreqB(oldDODtau,Nbig,beta)
+			else:
+				oldGDomega, oldGODomega = 1.0*GDomega, 1.0*GODomega
+				oldDDomega, oldDODomega = 1.0*DDomega, 1.0*DODomega
+			
+			SigmaDtau = 1.0 * kappa * (g**2) * DDtau * GDtau
+			SigmaODtau = 1.0 * kappa * (g**2) * DODtau * GODtau
+			PiDtau = 2.0 * g**2 * GDtau * GDtau[::-1] #KMS G(-tau) = -G(beta-tau)
+			PiODtau = 2.0 * g**2 * GODtau * GODtau[::-1] #KMS G(-tau) = -G(beta-tau)
+			
+			SigmaDomega, SigmaODomega = Time2FreqF(SigmaDtau,Nbig,beta),Time2FreqF(SigmaODtau,Nbig,beta)
+			PiDomega, PiODomega =  Time2FreqB(PiDtau,Nbig,beta), Time2FreqB(PiODtau,Nbig,beta)
+			
+			detG = (1j*omega+mu-SigmaDomega)**2 - (lamb - SigmaODomega)**2
+			detD = (nu**2 + r - PiDomega)**2 - (J - PiODomega)**2
+			GDomega = x*((1j*omega + mu - SigmaDomega)/(detG)) + (1-x)*oldGDomega
+			GODomega = x*(-1.*(lamb- SigmaODomega)/(detG)) + (1-x)*oldGODomega
+			DDomega = x*((nu**2 + r - PiDomega)/(detD)) + (1-x)*oldDDomega
+			DODomega = x*(-1.*(J- PiODomega)/(detD)) + (1-x)*oldDODomega
 
-	        GDtau = Freq2TimeF(GDomega,Nbig,beta)
-	        GODtau = Freq2TimeF(GODomega,Nbig,beta)
-	        DDtau = Freq2TimeB(DDomega,Nbig,beta)
-	        DODtau = Freq2TimeB(DODomega,Nbig,beta)
+			GDtau = Freq2TimeF(GDomega,Nbig,beta)
+			GODtau = Freq2TimeF(GODomega,Nbig,beta)
+			DDtau = Freq2TimeB(DDomega,Nbig,beta)
+			DODtau = Freq2TimeB(DODomega,Nbig,beta)
 
-	    
-	        diffGD = np.sum((np.abs(GDtau-oldGDtau))**2) 
-	        diffDOD = np.sum((np.abs(DODtau-oldDODtau))**2)
-	        diff = 0.5*(diffGD+diffDOD) # less stringent error metric - faster code
-	        # diffGD = np.sqrt(np.sum(np.abs(SigmaDtau - 1.0*kappa*(g**2)*DDtau*GDtau)**2))
-	        # diffDOD = np.sqrt(np.sum(np.abs(PiODtau - 2.0 * g**2 * GODtau * GODtau[::-1])**2))
-	        # diff = 0.5*(diffGD+diffDOD)
+		
+			diffGD = np.sum((np.abs(GDtau-oldGDtau))**2) 
+			diffDOD = np.sum((np.abs(DODtau-oldDODtau))**2)
+			diff = 0.5*(diffGD+diffDOD) # less stringent error metric - faster code
+			# diffGD = np.sqrt(np.sum(np.abs(SigmaDtau - 1.0*kappa*(g**2)*DDtau*GDtau)**2))
+			# diffDOD = np.sqrt(np.sum(np.abs(PiODtau - 2.0 * g**2 * GODtau * GODtau[::-1])**2))
+			# diff = 0.5*(diffGD+diffDOD)
 
-	    if calcfe == True:
-		    GFs = [GDomega,GODomega,DDomega,DODomega]
-		    BSEs = [PiDomega,PiODomega]
-		    freq_grids = [omega,nu] #used for free energy calculation
-		    fe = free_energy_rolling_YSYKWH(GFs,BSEs,freq_grids,Nbig,beta,g,r,mu,kappa)
-		    fe_list += [fe]
+		if calcfe == True:
+			GFs = [GDomega,GODomega,DDomega,DODomega]
+			BSEs = [PiDomega,PiODomega]
+			freq_grids = [omega,nu] #used for free energy calculation
+			fe = free_energy_rolling_YSYKWH(GFs,BSEs,freq_grids,Nbig,beta,g,r,mu,kappa)
+			fe_list += [fe]
 
-	    if DUMP == True and beta in [50,100,500,1000,2000,5000,10000,50000,100000]:
-	        savefile = 'Nbig' + str(int(np.log2(Nbig))) + 'beta' + str(beta) 
-	        savefile += 'lamb' + str(lamb) + 'J' + str(J)
-	        savefile += 'g' + str(g) + 'r' + str(r)
-	        savefile = savefile.replace('.','_') 
-	        savefile += '.npy'
-	        np.save(os.path.join(path_to_dump, savefile), np.array([GDtau,GODtau,DDtau,DODtau])) 
-	        if verbose==True: print(savefile)
+		if DUMP == True and beta in [50,100,500,1000,2000,5000,10000,50000,100000]:
+			savefile = 'Nbig' + str(int(np.log2(Nbig))) + 'beta' + str(beta) 
+			savefile += 'lamb' + str(lamb) + 'J' + str(J)
+			savefile += 'g' + str(g) + 'r' + str(r)
+			savefile = savefile.replace('.','_') 
+			savefile += '.npy'
+			np.save(os.path.join(path_to_dump, savefile), np.array([GDtau,GODtau,DDtau,DODtau])) 
+			if verbose==True: 
+				print(savefile)
 
-	    if verbose == True :
-	    	print("##### Finished beta = ", beta, "############")
-	    	#print("end x = ", x, " , end diff = ", diff,' , end itern = ',itern, '\n')
-	    	print("diff = ", diff,' , itern = ',itern, " , free energy = ", fe)
-	    beta = beta + beta_step
+		if verbose == True :
+			print("##### Finished beta = ", beta, "############")
+			#print("end x = ", x, " , end diff = ", diff,' , end itern = ',itern, '\n')
+			print(f'diff = {diff:.5}, itern = {itern}, free energy = {fe:.5}')
+		beta = beta + beta_step
 
-    return GDtau,GODtau,DDtau,DODtau,fe_list
-
-
-
-
-
-
-
-	
+	beta -= 1 
+	return GDtau,GODtau,DDtau,DODtau,fe_list
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-def anneal_lamb():
+def anneal_lamb(target_lamb,GFtaus,Nbig,beta_start,g,r,mu,lamb,J,kappa,DUMP=False,path_to_dump=None,calcfe=False,verbose=True):
 	return None
 
 
+############### Tests ##################
+
+def test_anneal_temp():
+	Nbig = int(2**14)
+	beta_start = 1 
+	target_beta = int(1e4)
+	beta = beta_start
+	mu = 0.0
+	g = 0.5
+	r = 1.
+	lamb = 0.005
+	J = 0
+	kappa = 1.
+	DUMP = False
+
+	omega = ImagGridMaker(Nbig,beta,'fermion')
+	nu = ImagGridMaker(Nbig,beta,'boson')
+	tau = ImagGridMaker(Nbig,beta,'tau')
+
+	Gfreetau = Freq2TimeF(1./(1j*omega + mu),Nbig,beta)
+	Dfreetau = Freq2TimeB(1./(nu**2 + r),Nbig,beta)
+	delta = 0.420374134464041
+	omegar2 = ret_omegar2(g,beta)
+
+	GDtau, DDtau = Gfreetau, Dfreetau
+	GODtau = Freq2TimeF(-lamb/((1j*omega+mu)**2 - lamb**2), Nbig, beta)
+	DODtau = Freq2TimeB(-J/(nu**2 + r)**2 - J**2, Nbig, beta)
+	GFtaus = [GDtau,GODtau,DDtau,DODtau]
+
+	GDtau,GODtau,DDtau,DODtau,fe_list = anneal_temp(target_beta,GFtaus,Nbig,beta_start,g,r,mu,lamb,J,kappa,
+											DUMP=DUMP,path_to_dump=None,calcfe=True,verbose=True)
+
+	beta = target_beta
+	omega = ImagGridMaker(Nbig,beta,'fermion')
+	nu = ImagGridMaker(Nbig,beta,'boson')
+	tau = ImagGridMaker(Nbig,beta,'tau')
+
+	Gconftau = Freq2TimeF(GconfImag(omega,g,beta),Nbig,beta)
+	Dconftau = Freq2TimeB(DconfImag(nu,g,beta),Nbig,beta)
+	FreeDtau = DfreeImagtau(tau,r,beta)
+
+	fig, ax = plt.subplots(2,2)
+
+	titlestring = r'$\beta$ = ' + str(beta) + r', $\log_2{N}$ = ' + str(np.log2(Nbig)) + r', $g = $' + str(g)
+	titlestring += r' $\lambda$ = ' + str(lamb) + r' J = ' + str(J)
+	fig.suptitle(titlestring)
+	fig.tight_layout(pad=2)
+	ax[0,0].plot(tau/beta, np.real(GDtau), 'r', label = 'numerics GDtau')
+	ax[0,0].plot(tau/beta, np.real(Gconftau), 'b--', label = 'analytical Gtau' )
+	#ax[0,0].set_ylim(-1,1)
+	ax[0,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	ax[0,0].set_ylabel(r'$\Re{GD(\tau)}$')
+	ax[0,0].legend()
+
+	ax[0,1].plot(tau/beta, np.real(GODtau), 'r', label = 'numerics Real GODtau')
+	ax[0,1].plot(tau/beta, np.imag(GODtau), 'k', label = 'numerics imag GODtau')
+	#ax[0,1].set_ylim(-1,1)
+	ax[0,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	ax[0,1].set_ylabel(r'$\Re{GOD(\tau)}$')
+	ax[0,1].legend()
+
+	ax[1,0].plot(tau/beta, np.real(DDtau), 'r', label = 'numerics DDtau')
+	ax[1,0].plot(tau/beta, np.real(Dconftau), 'b--', label = 'analytical Dtau' )
+	ax[1,0].plot(tau/beta, np.real(FreeDtau), 'g-.', label = 'Free D Dtau' )
+	#ax[1,0].set_ylim(0,1)
+	ax[1,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	ax[1,0].set_ylabel(r'$\Re{DD(\tau)}$')
+	ax[1,0].legend()
+
+	ax[1,1].plot(tau/beta, np.real(DODtau), 'r', label = 'numerics real DODtau')
+	ax[1,1].plot(tau/beta, np.imag(DODtau), 'k', label = 'numerics imag DODtau')
+	#ax[1,1].set_ylim(0,1)
+	ax[1,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	ax[1,1].set_ylabel(r'$\Re{DOD(\tau)}$')
+	ax[1,1].legend()
+
+	fig,ax = plt.subplots(1)
+	fig.suptitle('Free energy as a function of temp')
+	# ax.plot(np.arange(beta_start,target_beta,beta_step), fe_list)
+	ax.plot(np.arange(beta_start,target_beta+1), fe_list)
+	ax.set_ylabel('Free energy')
+	ax.set_xlabel(r'$\beta')
+
+	plt.show()
 
 
 
 
+
+if __name__ == '__main__': 
+	test_anneal_temp()
 
 
 
