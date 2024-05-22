7c08a0d50baf2b15e62bb7230c6a9c61f9ca1fa8
Shankar <55738744+aravindhshankar@users.noreply.github.com>
2024-05-22 15:26:25 +0200

    indentation issue in anneal_J

--- ClusterScript/WH_Sandbox/annealers.py
+++ ClusterScript/WH_Sandbox/annealers.py
@@ -269,122 +269,122 @@ def anneal_lamb(lamb_list,GFtaus,Nbig,g,r,mu,beta,J,kappa,DUMP=False,path_to_dum
 
 
 
-	def anneal_J(J_list,GFtaus,Nbig,g,r,mu,beta,lamb,kappa,DUMP=False,path_to_dump=None,savelist=None,calcfe=False,verbose=True):
-		if verbose: 
-			print("############ Started : target J = , ", J_list[-1], " #############")
-		GDtau,GODtau,DDtau,DODtau = GFtaus
+def anneal_J(J_list,GFtaus,Nbig,g,r,mu,beta,lamb,kappa,DUMP=False,path_to_dump=None,savelist=None,calcfe=False,verbose=True):
+	if verbose: 
+		print("############ Started : target J = , ", J_list[-1], " #############")
+	GDtau,GODtau,DDtau,DODtau = GFtaus
 
-		fe_list = [] #could be slow - try prealloacating for speed later
-		fe = 0.0
+	fe_list = [] #could be slow - try prealloacating for speed later
+	fe = 0.0
 
-		assert len(GDtau) == Nbig, 'Improperly loaded starting guess'
-		omega = ImagGridMaker(Nbig,beta,'fermion')
-		nu = ImagGridMaker(Nbig,beta,'boson')
-		tau = ImagGridMaker(Nbig,beta,'tau')
+	assert len(GDtau) == Nbig, 'Improperly loaded starting guess'
+	omega = ImagGridMaker(Nbig,beta,'fermion')
+	nu = ImagGridMaker(Nbig,beta,'boson')
+	tau = ImagGridMaker(Nbig,beta,'tau')
 
-		for J in J_list:
-			itern = 0
-			diff = err*1.1
-			diffG = 1.
-			diffD = 1.
-			# x = 0.01
-			# x = 0.5 if beta < 10 else 0.01
-			# x = 0.5 if beta < 5 else 1.
-			# x = 0.5 if beta < 5 else 0.01
-			x = 0.01
-			diff = 1.
-			iterni=0
-			conv_flag = False
-			# while(conv_flag == False):
-			while(diff>err and itern < ITERMAX):
-				# if diff < 10*err and itern > 2:
-				# 	x = 1.
-				diffold = diff
-				if itern == ITERMAX - 1: 
-					print(f"WARNING : CONVERGENCE NOT REACHED FOR BETA = {beta}, J = {J} in J ANNEAL")
-				itern+=1
-				iterni += 1 
+	for J in J_list:
+		itern = 0
+		diff = err*1.1
+		diffG = 1.
+		diffD = 1.
+		# x = 0.01
+		# x = 0.5 if beta < 10 else 0.01
+		# x = 0.5 if beta < 5 else 1.
+		# x = 0.5 if beta < 5 else 0.01
+		x = 0.01
+		diff = 1.
+		iterni=0
+		conv_flag = False
+		# while(conv_flag == False):
+		while(diff>err and itern < ITERMAX):
+			# if diff < 10*err and itern > 2:
+			# 	x = 1.
+			diffold = diff
+			if itern == ITERMAX - 1: 
+				print(f"WARNING : CONVERGENCE NOT REACHED FOR BETA = {beta}, J = {J} in J ANNEAL")
+			itern+=1
+			iterni += 1 
 
-				oldGDtau, oldGODtau = 1.0*GDtau, 1.0*GODtau
-				oldDDtau, oldDODtau = 1.0*DDtau, 1.0*DODtau
-				
-				if iterni == 1:
-					oldGDomega,oldGODomega = Time2FreqF(oldGDtau,Nbig,beta),Time2FreqF(oldGODtau,Nbig,beta)
-					oldDDomega, oldDODomega = Time2FreqB(oldDDtau,Nbig,beta),Time2FreqB(oldDODtau,Nbig,beta)
-				else:
-					oldGDomega, oldGODomega = 1.0*GDomega, 1.0*GODomega
-					oldDDomega, oldDODomega = 1.0*DDomega, 1.0*DODomega
-				
-				SigmaDtau = 1.0 * kappa * (g**2) * DDtau * GDtau
-				SigmaODtau = 1.0 * kappa * (g**2) * DODtau * GODtau
-				PiDtau = 2.0 * g**2 * GDtau * GDtau[::-1] #KMS G(-tau) = -G(beta-tau)
-				PiODtau = 2.0 * g**2 * GODtau * GODtau[::-1] #KMS G(-tau) = -G(beta-tau)
-				
-				SigmaDomega, SigmaODomega = Time2FreqF(SigmaDtau,Nbig,beta),Time2FreqF(SigmaODtau,Nbig,beta)
-				PiDomega, PiODomega =  Time2FreqB(PiDtau,Nbig,beta), Time2FreqB(PiODtau,Nbig,beta)
-				
-				detG = (1j*omega+mu-SigmaDomega)**2 - (lamb - SigmaODomega)**2
-				detD = (nu**2 + r - PiDomega)**2 - (J - PiODomega)**2
-				GDomega = x*((1j*omega + mu - SigmaDomega)/(detG)) + (1-x)*oldGDomega
-				GODomega = x*(-1.*(lamb- SigmaODomega)/(detG)) + (1-x)*oldGODomega
-				DDomega = x*((nu**2 + r - PiDomega)/(detD)) + (1-x)*oldDDomega
-				DODomega = x*(-1.*(J- PiODomega)/(detD)) + (1-x)*oldDODomega
-
-				GDtau = Freq2TimeF(GDomega,Nbig,beta)
-				GODtau = Freq2TimeF(GODomega,Nbig,beta)
-				DDtau = Freq2TimeB(DDomega,Nbig,beta)
-				DODtau = Freq2TimeB(DODomega,Nbig,beta)
-
+			oldGDtau, oldGODtau = 1.0*GDtau, 1.0*GODtau
+			oldDDtau, oldDODtau = 1.0*DDtau, 1.0*DODtau
 			
-				diffGD = np.sum((np.abs(GDtau-oldGDtau))**2) 
-				diffDD = np.sum((np.abs(DDtau-oldDDtau))**2)
-				diff = 0.5*(diffGD+diffDD) # less stringent error metric - faster code
-				# diffGD = np.sqrt(np.sum(np.abs(SigmaDtau - 1.0*kappa*(g**2)*DDtau*GDtau)**2))
-				# diffDOD = np.sqrt(np.sum(np.abs(PiODtau - 2.0 * g**2 * GODtau * GODtau[::-1])**2))
-				# diff = 0.5*(diffGD+diffDOD)
-				# if (diff>err or np.isclose(x,1.)==False) and itern < ITERMAX:
-				# 	if diff > diffold and x*0.5 > 0.01 :
-				# 		x *= 0.5
-				# 	elif diff < diffold and x*1.1 <= 1. :
-				# 		x *= 1.1
-				# 	if diff < 1e-8 and x > 0.5:
-				# 		x = 1.
-				# 	if x > 0.9:
-				# 		x = 1.
-				# 	if diff < err: 
-				# 		x = 1.
-				# else: 
-				# 	conv_flag = True
-				# if diff < 1e-8 and x > 0.5:
-				# 	x = 1.
-				# if x > 0.9:
-				# 	x = 1.
-				# if diff < err: 
-				# 	x = 1.
+			if iterni == 1:
+				oldGDomega,oldGODomega = Time2FreqF(oldGDtau,Nbig,beta),Time2FreqF(oldGODtau,Nbig,beta)
+				oldDDomega, oldDODomega = Time2FreqB(oldDDtau,Nbig,beta),Time2FreqB(oldDODtau,Nbig,beta)
+			else:
+				oldGDomega, oldGODomega = 1.0*GDomega, 1.0*GODomega
+				oldDDomega, oldDODomega = 1.0*DDomega, 1.0*DODomega
+			
+			SigmaDtau = 1.0 * kappa * (g**2) * DDtau * GDtau
+			SigmaODtau = 1.0 * kappa * (g**2) * DODtau * GODtau
+			PiDtau = 2.0 * g**2 * GDtau * GDtau[::-1] #KMS G(-tau) = -G(beta-tau)
+			PiODtau = 2.0 * g**2 * GODtau * GODtau[::-1] #KMS G(-tau) = -G(beta-tau)
+			
+			SigmaDomega, SigmaODomega = Time2FreqF(SigmaDtau,Nbig,beta),Time2FreqF(SigmaODtau,Nbig,beta)
+			PiDomega, PiODomega =  Time2FreqB(PiDtau,Nbig,beta), Time2FreqB(PiODtau,Nbig,beta)
+			
+			detG = (1j*omega+mu-SigmaDomega)**2 - (lamb - SigmaODomega)**2
+			detD = (nu**2 + r - PiDomega)**2 - (J - PiODomega)**2
+			GDomega = x*((1j*omega + mu - SigmaDomega)/(detG)) + (1-x)*oldGDomega
+			GODomega = x*(-1.*(lamb- SigmaODomega)/(detG)) + (1-x)*oldGODomega
+			DDomega = x*((nu**2 + r - PiDomega)/(detD)) + (1-x)*oldDDomega
+			DODomega = x*(-1.*(J- PiODomega)/(detD)) + (1-x)*oldDODomega
 
-			if calcfe == True:
-				GFs = [GDomega,GODomega,DDomega,DODomega]
-				BSEs = [PiDomega,PiODomega]
-				freq_grids = [omega,nu] #used for free energy calculation
-				fe = free_energy_rolling_YSYKWH(GFs,BSEs,freq_grids,Nbig,beta,g,r,mu,kappa)
-				fe_list += [fe]
+			GDtau = Freq2TimeF(GDomega,Nbig,beta)
+			GODtau = Freq2TimeF(GODomega,Nbig,beta)
+			DDtau = Freq2TimeB(DDomega,Nbig,beta)
+			DODtau = Freq2TimeB(DODomega,Nbig,beta)
 
-			if DUMP == True and np.isclose(savelist,J).any():
-				Jval = savelist[np.isclose(savelist,J)][0]
-				savefile = 'Nbig' + str(int(np.log2(Nbig))) + 'beta' + str(beta) 
-				savefile += 'lamb' + f'{lamb:.3}' + 'J' + f'{Jval:.3}'
-				savefile += 'g' + str(g) + 'r' + str(r)
-				savefile = savefile.replace('.','_') 
-				savefile += '.npy'
-				np.save(os.path.join(path_to_dump, savefile), np.array([GDtau,GODtau,DDtau,DODtau])) 
-				if verbose==True: 
-					print(savefile)
-
-			if verbose == True :
-				print(f"##### Finished J =  {J} ############")
-				print(f'diff = {diff:.5}, itern = {itern}, free energy = {fe:.5}, x = {x:.3}',flush=True)
 		
-		return GDtau,GODtau,DDtau,DODtau,fe_list
+			diffGD = np.sum((np.abs(GDtau-oldGDtau))**2) 
+			diffDD = np.sum((np.abs(DDtau-oldDDtau))**2)
+			diff = 0.5*(diffGD+diffDD) # less stringent error metric - faster code
+			# diffGD = np.sqrt(np.sum(np.abs(SigmaDtau - 1.0*kappa*(g**2)*DDtau*GDtau)**2))
+			# diffDOD = np.sqrt(np.sum(np.abs(PiODtau - 2.0 * g**2 * GODtau * GODtau[::-1])**2))
+			# diff = 0.5*(diffGD+diffDOD)
+			# if (diff>err or np.isclose(x,1.)==False) and itern < ITERMAX:
+			# 	if diff > diffold and x*0.5 > 0.01 :
+			# 		x *= 0.5
+			# 	elif diff < diffold and x*1.1 <= 1. :
+			# 		x *= 1.1
+			# 	if diff < 1e-8 and x > 0.5:
+			# 		x = 1.
+			# 	if x > 0.9:
+			# 		x = 1.
+			# 	if diff < err: 
+			# 		x = 1.
+			# else: 
+			# 	conv_flag = True
+			# if diff < 1e-8 and x > 0.5:
+			# 	x = 1.
+			# if x > 0.9:
+			# 	x = 1.
+			# if diff < err: 
+			# 	x = 1.
+
+		if calcfe == True:
+			GFs = [GDomega,GODomega,DDomega,DODomega]
+			BSEs = [PiDomega,PiODomega]
+			freq_grids = [omega,nu] #used for free energy calculation
+			fe = free_energy_rolling_YSYKWH(GFs,BSEs,freq_grids,Nbig,beta,g,r,mu,kappa)
+			fe_list += [fe]
+
+		if DUMP == True and np.isclose(savelist,J).any():
+			Jval = savelist[np.isclose(savelist,J)][0]
+			savefile = 'Nbig' + str(int(np.log2(Nbig))) + 'beta' + str(beta) 
+			savefile += 'lamb' + f'{lamb:.3}' + 'J' + f'{Jval:.3}'
+			savefile += 'g' + str(g) + 'r' + str(r)
+			savefile = savefile.replace('.','_') 
+			savefile += '.npy'
+			np.save(os.path.join(path_to_dump, savefile), np.array([GDtau,GODtau,DDtau,DODtau])) 
+			if verbose==True: 
+				print(savefile)
+
+		if verbose == True :
+			print(f"##### Finished J =  {J} ############")
+			print(f'diff = {diff:.5}, itern = {itern}, free energy = {fe:.5}, x = {x:.3}',flush=True)
+	
+	return GDtau,GODtau,DDtau,DODtau,fe_list
 
 
 
