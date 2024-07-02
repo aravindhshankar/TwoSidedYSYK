c87de2b7d3aaa573eedcf025a573c332e3113e37
Shankar <55738744+aravindhshankar@users.noreply.github.com>
2024-07-02 17:17:40 +0200

    added mathematica notebook

--- ClusterScript/WH_Sandbox/PlotterProgImagWH.py
+++ ClusterScript/WH_Sandbox/PlotterProgImagWH.py
@@ -18,7 +18,8 @@ if not os.path.exists('../Dump/'):
 	raise Exception("Error - Path to Dump directory not found ")
 	exit(1)
 else:
-	path_to_dump = '../Dump/lamb_anneal_dumpfiles/'
+	path_to_dump = '../Dump/WHYSYKImagDumpfiles/SCWH'
+	# path_to_dump = '../Dump/lamb_anneal_dumpfiles/'
 	# path_to_dump_lamb = '../Dump/xshift_lamb_anneal_dumpfiles/'
 	# path_to_dump_temp = '../Dump/temp_anneal_dumpfiles/'
 	# path_to_dump_temp = '../Dump/xshift_temp_anneal_dumpfiles/'
@@ -48,14 +49,15 @@ err = 1e-5
 
 global beta
 
-beta = 500
+beta = 50.0
 mu = 0.0
-g = 0.5
+# g = 0.5
+g = 2.
 r = 1.
 
 kappa = 1.
-lamb = 0.01
-J = 0
+lamb = 0.05
+J = 0.0
 
 # path_to_dump = path_to_dump_lamb
 # path_to_dump = path_to_dump_temp
@@ -70,8 +72,9 @@ try :
     #plotfile = os.path.join(path_to_dump, 'Nbig14beta100_0lamb0_05J0_05g0_5r1_0.npy')
     plotfile = os.path.join(path_to_dump, savefile)
 except FileNotFoundError: 
-    print("INPUT FILE NOT FOUND") 
-    exit(1)
+	print('Filename : ', savefile)
+	print("INPUT FILE NOT FOUND") 
+	exit(1)
 
 print('savefile = ', savefile)
 
