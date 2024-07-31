f7e8515597e14050bb99b5749d3dfe42f302bb68
Shankar <55738744+aravindhshankar@users.noreply.github.com>
2024-07-31 15:58:59 +0200

    transfercomm

--- ClusterScript/RTgapplottingnpy.py
+++ ClusterScript/RTgapplottingnpy.py
@@ -16,8 +16,29 @@ import matplotlib.pyplot as plt
 path_to_outfile = './Dump/RTGapscaling/'
 path_to_BH = './Dump/RTWHDumpfiles0_00/'
 
-outfile = ''
+# outfile = 'RTgapM16T12beta200g0_5lamb0_06.npy'
 BH_outfile = 'l_00M16T12beta200g0_5lamb0_0.npy'
+
+M=int(2**16)
+T=2**12
+beta = 200
+temp=1./beta
+g = 0.5
+lamb = 0.06
+J = 0.
+# GDomega,GODomega,DDomega,DODomega = np.load(savepath)
+
+
+savefile = 'RTgap'
+savefile += 'M' + str(int(np.log2(M))) + 'T' + str(int(np.log2(T))) 
+# savefile += 'beta' + str((round(beta*100))/100.) 
+savefile += 'beta' + str(beta) 
+savefile += 'g' + str(g)
+savefile += 'lamb' + f'{lamb:.3}'
+savefile = savefile.replace('.','_') 
+savefiledump = savefile + '.npy'
+outfile = savefiledump
+
 
 savepath = os.path.join(path_to_outfile, outfile)
 BHsavepath = os.path.join(path_to_BH, BH_outfile)
@@ -26,19 +47,8 @@ if not os.path.exists(savepath):
 if not os.path.exists(BHsavepath):
 	raise(Exception("BH Output file not found"))
 
-
-M=int(2**16)
-T=2**12
-beta = 200.
-temp=1./beta
-g = 0.5
-lamb = 0.01
-J = 0.
-# GDomega,GODomega,DDomega,DODomega = np.load(savepath)
 loaded = np.array(np.load(savepath))
 loaded_BH = np.array(np.load(BHsavepath))
-
-
 
 # print(len(loaded))
 # print(len(loaded[2]), 2*M)
@@ -51,7 +61,7 @@ omega,t  = RealGridMaker(M,T)
 dt = t[2]-t[1]
 
 idx = 1
-fig,ax  = plt.subplots(1)
+
 to_plot = -np.imag(loaded[idx])
 BH_to_plot = -np.imag(loaded_BH[idx])
 
@@ -69,29 +79,30 @@ trans_am_OD = 2 * np.abs(G_great_OD)
 # deltarho = np.abs(to_plot-BH_to_plot) / BH_to_plot
 deltarho = to_plot/ BH_to_plot
 
+# fig,ax  = plt.subplots(1)
 # xaxis = omega
-xaxis = omega/lambexpo
-ax.set_xlabel(r'$\frac{\omega}{\lambda^{1/(2-2\Delta)}}$')
-# ax.plot(xaxis,to_plot)
-# ax.plot(xaxis,BH_to_plot,'--')
-ax.plot(xaxis,deltarho)
-# ax.set_ylim(0,10)
-# ax.set_xlim(-5,5)
-# ax.set_xlim(-20,50)
-print(f'min BH = {min(BH_to_plot)}')
-print(f'min WH = {min(to_plot)}')
-peakpoints = find_peaks(deltarho,prominence=0.1)[0]
-peakvals = [xaxis[peak] for peak in peakpoints if peak>=M]
-print(f'peakvals = {peakvals}')
-print(f'diffs = {np.diff(peakvals)}')
-# ax.axvline(g**(2./3), ls= '--',c='blue',label=r'$g^{2/3}$')
-# ax.axvline(1./beta,ls='--',c='magenta',label=r'temperature')
-ax.axvline(g**(2./3)/lambexpo, ls= '--',c='blue',label=r'$g^{2/3}$')
-ax.axvline(temp/lambexpo,ls='--',c='magenta',label=r'temperature')
-for peak in peakvals:
-    ax.axvline(peak,ls='--',c='gray')
-ax.legend()
-# ax.set_xticks(np.arange(-20,50,1))
+# xaxis = omega/lambexpo
+# ax.set_xlabel(r'$\frac{\omega}{\lambda^{1/(2-2\Delta)}}$')
+# # ax.plot(xaxis,to_plot)
+# # ax.plot(xaxis,BH_to_plot,'--')
+# ax.plot(xaxis,deltarho)
+# # ax.set_ylim(0,10)
+# # ax.set_xlim(-5,5)
+# # ax.set_xlim(-20,50)
+# print(f'min BH = {min(BH_to_plot)}')
+# print(f'min WH = {min(to_plot)}')
+# peakpoints = find_peaks(deltarho,prominence=0.1)[0]
+# peakvals = [xaxis[peak] for peak in peakpoints if peak>=M]
+# print(f'peakvals = {peakvals}')
+# print(f'diffs = {np.diff(peakvals)}')
+# # ax.axvline(g**(2./3), ls= '--',c='blue',label=r'$g^{2/3}$')
+# # ax.axvline(1./beta,ls='--',c='magenta',label=r'temperature')
+# ax.axvline(g**(2./3)/lambexpo, ls= '--',c='blue',label=r'$g^{2/3}$')
+# ax.axvline(temp/lambexpo,ls='--',c='magenta',label=r'temperature')
+# for peak in peakvals:
+#     ax.axvline(peak,ls='--',c='gray')
+# ax.legend()
+# # ax.set_xticks(np.arange(-20,50,1))
 
 
 fig,ax = plt.subplots(1)
@@ -119,7 +130,13 @@ BHrhoGOD = -np.imag(BHGODomega)
 BHrhoDD = -np.imag(BHDDomega)
 BHrhoDOD = -np.imag(BHDODomega)
 
+peaks = find_peaks(rhoGD,prominence=1)[0]
+# print(peaks)
+peakvals = omega[peaks]
+print(peakvals)
 
+peakmax = omega[M + np.argmax(rhoGD[M:])]
+print(peakmax)
 
 fig, ax = plt.subplots(2,2)
 titlestring = 'beta = ' + str(beta) + ' lamb = ' + str(lamb) + ' J = ' + str(J)
@@ -129,6 +146,9 @@ fig.suptitle(titlestring)
 
 ax[0,0].plot(omega, rhoGD)
 ax[0,0].plot(omega, BHrhoGD, '--', label='BH')
+# ax[0,0].loglog(omega, rhoGD)
+# ax[0,0].loglog(omega, BHrhoGD, '--', label='BH')
+# ax[0,0].axvline(peakvals[::-1])
 ax[0,0].set_xlim(-5,5)
 ax[0,0].set_title(r'rho GD')
 delta = 0.420374134464041
@@ -148,29 +168,35 @@ ax[1,1].plot(omega, rhoDOD)
 ax[1,1].set_title(r'rho DOD')
 ax[1,1].plot(omega, BHrhoDOD, '--', label='BH')
 
+
+
+
+
+
+
 ############# log log plot ###################
 
-fig, ax = plt.subplots(1)
-start = len(omega)//2 + 1 
-stop = start + 500
-temp_idx = np.argmin(np.abs(temp - omega))
-fitslice = slice(temp_idx+40, temp_idx + 50)
-#fitslice = slice(start+25, start + 35)
+# fig, ax = plt.subplots(1)
+# start = len(omega)//2 + 1 
+# stop = start + 500
+# temp_idx = np.argmin(np.abs(temp - omega))
+# fitslice = slice(temp_idx+40, temp_idx + 50)
+# #fitslice = slice(start+25, start + 35)
 
-functoplot = rhoGD
-m,c = np.polyfit(np.log(np.abs(omega[fitslice])), np.log(functoplot[fitslice]),1)
-print(f'slope of fit = {m:.03f}')
-print('2 Delta - 1 = ', 2*delta-1)
+# functoplot = rhoGD
+# m,c = np.polyfit(np.log(np.abs(omega[fitslice])), np.log(functoplot[fitslice]),1)
+# print(f'slope of fit = {m:.03f}')
+# print('2 Delta - 1 = ', 2*delta-1)
 
-ax.loglog(omega[start:stop], functoplot[start:stop],'p',label = 'numerics rhoGDomega')
-ax.loglog(omega[start:stop], np.exp(c)*np.abs(omega[start:stop])**m, label=f'Fit with slope {m:.03f}')
-ax.axvline((temp,),ls='--')
-#ax.set_ylim(1e-1,1e1)
-ax.set_xlabel(r'$\omega$')
-ax.set_ylabel(r'$-\Im{GD(\omega)}$')
-#ax.set_aspect('equal', adjustable='box')
-#ax.axis('square')
-ax.legend()
+# ax.loglog(omega[start:stop], functoplot[start:stop],'p',label = 'numerics rhoGDomega')
+# ax.loglog(omega[start:stop], np.exp(c)*np.abs(omega[start:stop])**m, label=f'Fit with slope {m:.03f}')
+# ax.axvline((temp,),ls='--')
+# #ax.set_ylim(1e-1,1e1)
+# ax.set_xlabel(r'$\omega$')
+# ax.set_ylabel(r'$-\Im{GD(\omega)}$')
+# #ax.set_aspect('equal', adjustable='box')
+# #ax.axis('square')
+# ax.legend()
 
 
 
