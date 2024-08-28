050e1602230fafece2c6a3bbf55303e1a5b5418d
Shankar <55738744+aravindhshankar@users.noreply.github.com>
2024-08-28 22:14:06 +0200

    God zijn Christus

--- ClusterScript/Figuremaker/v2GreenFunctionPlotsMetal.py
+++ ClusterScript/Figuremaker/v2GreenFunctionPlotsMetal.py
@@ -31,20 +31,32 @@ from SYK_fft import *
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
+from matplotlib.ticker import ScalarFormatter,NullFormatter
 from ConformalAnalytical import *
 from Insethelpers import add_subplot_axes
 #import time
 
 plt.style.use('physrev.mplstyle') # Set full path to if physrev.mplstyle is not in the same in directory as the notebook
 plt.rcParams['figure.dpi'] = "120"
-plt.rcParams['figure.figsize'] = '8 ,7'
+# plt.rcParams['figure.figsize'] = '8 ,7'
 plt.rcParams['lines.markersize'] = '6'
-plt.rcParams['lines.linewidth'] = '1.6'
-plt.rcParams['axes.labelsize'] = '16'
-plt.rcParams['axes.titlesize'] = '18'
-plt.rcParams['figure.titlesize'] = '20'
+plt.rcParams['lines.linewidth'] = '1.2'
+plt.rcParams['axes.labelsize'] = '12'
+plt.rcParams['axes.titlesize'] = '12'
+plt.rcParams['figure.titlesize'] = '12'
+# plt.rcParams['text.latex.preamble']=r'\usepackage{cmbright}'
 
-plt.rcParams['legend.fontsize'] = '12'
+# plt.rcParams['legend.fontsize'] = '12'
+# # # plt.rcParams['legend.fontsize'] = '14'
+# plt.rcParams['figure.titlesize'] = '10'
+# plt.rcParams['axes.titlesize'] = '10'
+# plt.rcParams['axes.labelsize'] = '10'
+# plt.rcParams['figure.figsize'] = f'{3.25*2},{3.25*2}'
+# # plt.rcParams['lines.markersize'] = '6'
+# plt.rcParams['lines.linewidth'] = '0.5'
+plt.rcParams['axes.formatter.limits'] = '-2,2'
+
+# plt.rcParams['figure.figsize'] = '8,7'
 # print(plt.rcParams.keys())
 
 Nbig = int(2**14)
@@ -70,48 +82,72 @@ J = 0
 path_to_dump = path_to_dump_temp
 
 
-fig, ax = plt.subplots(2,2, figsize=(8,7))
+# fig, ax = plt.subplots(2,2, figsize=(8,7))
 titlestring = "Imaginary time Green's functions for "
-# titlestring +=  r', $g = $' + str(g)
+# # titlestring +=  r', $g = $' + str(g)
 titlestring += r' $\lambda$ = ' + str(lamb) 
-fig.suptitle(titlestring)
-fig.tight_layout(pad=3)
-for axi in ax:
-	for axj in axi:
-		axj.tick_params(labelsize=7)
+# fig.suptitle(titlestring)
+# fig.tight_layout(pad=3)
+# for axi in ax:
+# 	for axj in axi:
+# 		axj.tick_params(labelsize=7)
 
 # GDinsetax.set_xlabel(r'$\tau/\beta$',labelpad = 0, fontsize=8)
 # GDinsetax.tick_params(axis='both', labelsize=4,pad=0.5)
 
 
-figSL,axSL = plt.subplots(2,2)
+figSL,axSL = plt.subplots(2,2, constrained_layout=True)
+figSL.set_figwidth(3.25*2)
+figSL.set_figwidth(3.25*2.2)
 figSL.suptitle(titlestring)
-figSL.tight_layout(pad=2)
+# figSL.tight_layout(pad=2)
 axSL[0,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-axSL[0,0].set_ylabel(r'$|\Re{G_{d}(\tau)}|$',labelpad = 0)
+axSL[0,0].set_ylabel(r'$|\Re{G_{d}(\tau)}|$',labelpad = 1)
 
 axSL[0,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-axSL[0,1].set_ylabel(r'$|\Re{G_{od}(\tau)}|$',labelpad = 0)
+axSL[0,1].set_ylabel(r'$|\Re{G_{od}(\tau)}|$',labelpad = 1)
 
 axSL[1,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-axSL[1,0].set_ylabel(r'$|\Re{D_{d}(\tau)}|$',labelpad = 0)
+axSL[1,0].set_ylabel(r'$|\Re{D_{d}(\tau)}|$',labelpad = 1)
 
 axSL[1,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-axSL[1,1].set_ylabel(r'$|\Re{D_{od}(\tau)}|$',labelpad = -20)
+axSL[1,1].set_ylabel(r'$|\Re{D_{od}(\tau)}|$',labelpad = 1)
 
-GDinsetax = add_subplot_axes(axSL[0,0], [0.1,0.1,0.3,0.3])
-DDinsetax = add_subplot_axes(axSL[1,0], [0.05,0.09,0.25,0.25])
-GODinsetax = add_subplot_axes(axSL[0,1], [0.1,0.4,0.3,0.3])
-DODinsetax = add_subplot_axes(axSL[1,1], [0.1,0.3,0.25,0.25])
+# GDinsetax = add_subplot_axes(axSL[0,0], [0.1,0.1,0.5,0.5])
+# GODinsetax = add_subplot_axes(axSL[0,1], [0.1,0.4,0.5,0.5])
+# DDinsetax = add_subplot_axes(axSL[1,0], [0.05,0.09,0.5,0.5])
+# DODinsetax = add_subplot_axes(axSL[1,1], [0.1,0.3,0.5,0.5])
+
+GDinsetax = add_subplot_axes(axSL[0,0], [-0.08,-0.064,0.4,0.4])
+GODinsetax = add_subplot_axes(axSL[0,1], [0.1,0.0,0.4,0.4])
+DDinsetax = add_subplot_axes(axSL[1,0], [0.5,0.35,0.4,0.4])
+DODinsetax = add_subplot_axes(axSL[1,1], [0.6,0.25,0.4,0.4])
+insetaxes = [GDinsetax,GODinsetax,DDinsetax,DODinsetax]
+
+
+
+# GDinsetax.set_xticklabels([])
+# 	insetax.set_xlabel('')
+insetfontsize = 7
+insetlabelpad = -5
+GDinsetax.set_xlabel(r'$\omega_n$',fontsize=insetfontsize,labelpad=-5)
+GDinsetax.set_ylabel(r'$-\Im{G_d}(\omega_n)$',fontsize=insetfontsize,labelpad=-4)
+GODinsetax.set_xlabel(r'$\omega_n$',fontsize=insetfontsize,labelpad=-4)
+GODinsetax.set_ylabel(r'$G_{od}(\omega_n)$',fontsize=insetfontsize,labelpad=-3)
+DODinsetax.set_xlabel(r'$\nu_n$',fontsize=insetfontsize,labelpad=-3)
+DODinsetax.set_ylabel(r'$D_{od}(\nu_n)$',labelpad = 1,fontsize=insetfontsize)
+DDinsetax.set_xlabel(r'$\nu_n$',fontsize=insetfontsize,labelpad=-3)
+DDinsetax.set_ylabel(r'$\Re{D_d}(\nu_n)$',labelpad = -1,fontsize=insetfontsize)
+# DODinsetax.set_ylabel(r'$|\Re{D_{od}(\tau)}|$')
 
 for axi in axSL:
 	for axj in axi:
-		axj.tick_params(axis='both', labelsize=7,pad=0.5)
+		axj.tick_params(axis='both', labelsize=10,pad=0.5)
+		# axj.set_box_aspect(aspect=1)
 
-
-figLL,axLL = plt.subplots(2,2)
-figLL.suptitle(titlestring)
-figLL.tight_layout(pad=2)
+# figLL,axLL = plt.subplots(2,2)
+# figLL.suptitle(titlestring)
+# figLL.tight_layout(pad=2)
 
 
 
@@ -163,104 +199,51 @@ for i, beta in enumerate(betalist):
 	GODomega = Time2FreqF(GODtau, Nbig, beta)
 	DDomega = Time2FreqB(DDtau, Nbig, beta)
 	DODomega = Time2FreqB(DODtau, Nbig, beta)
+	Gomega = Time2FreqF(Gtau, Nbig, beta)
+	Domega = Time2FreqB(Dtau, Nbig, beta)
 
 
-	################## PLOTTING ######################
-	print(beta), print(tau[-1])
-	Gconftau = Freq2TimeF(GconfImag(omega,g,beta),Nbig,beta)
-	Dconftau = Freq2TimeB(DconfImag(nu,g,beta),Nbig,beta)
-	FreeDtau = DfreeImagtau(tau,r,beta)
-
-
-
-	ax[0,0].plot(tau/beta, np.real(GDtau), c=col, ls = '-', label = lab)
-	ax[0,0].plot(tau/beta, np.real(Gtau), c=col, ls='--' )
-	ax[0,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-	ax[0,0].set_ylabel(r'$\Re{G_{d}(\tau)}$')
-	ax[0,0].legend()
-
-	ax[0,1].plot(tau/beta, np.real(GODtau), c = col, ls='-',  label = lab)
-	ax[0,1].plot(tau/beta, np.zeros_like(GODtau), c= col, ls = '--')
-
-	ax[0,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-	ax[0,1].set_ylabel(r'$\Re{G_{od}(\tau)}$')
-	ax[0,1].legend()
-
-	ax[1,0].plot(tau/beta, np.real(DDtau), c = col, ls='-', label = lab)
-	ax[1,0].plot(tau/beta, np.real(Dtau), c = col, ls= '--')
-
-	ax[1,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-	ax[1,0].set_ylabel(r'$\Re{D_{d}(\tau)}$')
-	ax[1,0].legend()
-
-	ax[1,1].plot(tau/beta, np.real(DODtau), c = col, ls='-', label = lab)
-	ax[1,1].plot(tau/beta, np.zeros_like(DODtau), c = col, ls='--')
-	ax[1,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
-	ax[1,1].set_ylabel(r'$\Re{D_{od}(\tau)}$')
-	ax[1,1].legend()
-
-	#fig.suptitle(r'$\beta$ = ', beta)
-	#plt.savefig('../../KoenraadEmails/Withx0_01constImagTime.pdf',bbox_inches='tight')
-	#plt.show()
+	# ################## PLOTTING ######################
+	# print(beta), print(tau[-1])
+	# Gconftau = Freq2TimeF(GconfImag(omega,g,beta),Nbig,beta)
+	# Dconftau = Freq2TimeB(DconfImag(nu,g,beta),Nbig,beta)
+	# FreeDtau = DfreeImagtau(tau,r,beta)
 
 
 
+	# ax[0,0].plot(tau/beta, np.real(GDtau), c=col, ls = '-', label = lab)
+	# ax[0,0].plot(tau/beta, np.real(Gtau), c=col, ls='--' )
+	# ax[0,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	# ax[0,0].set_ylabel(r'$\Re{G_{d}(\tau)}$')
+	# ax[0,0].legend()
+
+	# ax[0,1].plot(tau/beta, np.real(GODtau), c = col, ls='-',  label = lab)
+	# ax[0,1].plot(tau/beta, np.zeros_like(GODtau), c= col, ls = '--')
+
+	# ax[0,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	# ax[0,1].set_ylabel(r'$\Re{G_{od}(\tau)}$')
+	# ax[0,1].legend()
+
+	# ax[1,0].plot(tau/beta, np.real(DDtau), c = col, ls='-', label = lab)
+	# ax[1,0].plot(tau/beta, np.real(Dtau), c = col, ls= '--')
+
+	# ax[1,0].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	# ax[1,0].set_ylabel(r'$\Re{D_{d}(\tau)}$')
+	# ax[1,0].legend()
+
+	# ax[1,1].plot(tau/beta, np.real(DODtau), c = col, ls='-', label = lab)
+	# ax[1,1].plot(tau/beta, np.zeros_like(DODtau), c = col, ls='--')
+	# ax[1,1].set_xlabel(r'$\tau/\beta$',labelpad = 0)
+	# ax[1,1].set_ylabel(r'$\Re{D_{od}(\tau)}$')
+	# ax[1,1].legend()
+
+	# #fig.suptitle(r'$\beta$ = ', beta)
+	# #plt.savefig('../../KoenraadEmails/Withx0_01constImagTime.pdf',bbox_inches='tight')
+	# #plt.show()
 
 
 
-	############## LOG-LOG POWER LAW PLOT #####################
-
-	start, stop = Nbig//2, Nbig//2 + 100
-	startB, stopB = Nbig//2 + 1 , Nbig//2 + 101
-	delta = 0.420374134464041
-	alt_delta = 0.116902  
-
-	fitGD_val = -np.imag(GDomega[start+0])*(g**2)
-	#fitGD_val = -np.imag(Gconf[start:stop])*(g**2)
-	conf_fit_GD = 1 * np.abs(omega/(g**2))**(2*delta - 1)
-	conf_fit_GD = conf_fit_GD/conf_fit_GD[start] * fitGD_val
-
-	fitDD_val = np.real(DDomega[startB])*(g**2)
-	#fitDD_val = np.real(Dconf[startB:stopB])
-	conf_fit_DD = 1 * np.abs(nu[startB:stopB]/(g**2))**(1-4*delta)
-	conf_fit_DD = conf_fit_DD/conf_fit_DD[0] * fitDD_val
-
-	fitslice = slice(start+0, start + 15)
-	#fitslice = slice(start+25, start + 35)
-	functoplot = -np.imag(GDomega)*(g**2)
-	m,c = np.polyfit(np.log(np.abs(omega[fitslice])/(g**2)), np.log(functoplot[fitslice]),1)
-	print(f'slope of fit = {m:.03f}')
-	print('2 Delta - 1 = ', 2*delta-1)
-
-	axLL[0,0].loglog(omega[start:stop]/(g**2), -np.imag(GDomega[start:stop])*(g**2),'p',c=col, label = lab)
-	# axLL[0,0].loglog(omega[start:stop]/(g**2), conf_fit_GD[start:stop],'k--',label = 'ES power law')
-
-	# axLL[0,0].loglog(omega[start:stop]/(g**2), np.exp(c)*np.abs(omega[start:stop]/(g**2))**m, label=f'Fit with slope {m:.03f}')
-	axLL[0,0].set_xlabel(r'$\omega_n/g^2$')
-	axLL[0,0].set_ylabel(r'$-g^2\,\Im{GD(\omega_n)}$')
-
-	axLL[0,0].legend()
-
-
-	# axLL[0,1].loglog(omega[start:stop]/(g**2), np.imag(GODomega[start:stop])*(g**2),'p',label = 'numerics Im GODomega')
-	axLL[0,1].loglog(omega[start:stop]/(g**2), np.abs(GODomega[start:stop])*(g**2),'p',c=col,label = lab)
-	axLL[0,1].set_xlabel(r'$\omega_n/g^2$')
-	axLL[0,1].set_ylabel(r'$g^2\,GOD(\omega_n)$')
-	axLL[0,1].legend()
-
-	axLL[1,0].loglog(nu[startB:stopB]/(g**2), np.real(DDomega[startB:stopB])*(g**2),'p',c=col,label=lab)
-	# axLL[1,0].loglog(nu[startB:stopB]/(g**2), conf_fit_DD,'k--',label = 'ES power law')
-	axLL[1,0].set_xlabel(r'$\nu_n/g^2$')
-	axLL[1,0].set_ylabel(r'$g^2\,\Re{DD(\nu_n)}$',labelpad = None)
-	axLL[1,0].legend()
-
-
-	axLL[1,1].loglog(nu[startB:stopB]/(g**2), -np.real(DODomega[startB:stopB])*(g**2),'p',c=col,label=lab)
-	axLL[1,1].set_xlabel(r'$\nu_n/g^2$')
-	axLL[1,1].set_ylabel(r'$g^2\,DOD(\nu_n)$',labelpad = None)
-	axLL[1,1].legend()
-
-
+	
 
 	###################### Log-Linear Plot ###############################
 
@@ -268,6 +251,10 @@ for i, beta in enumerate(betalist):
 
 	startT, stopT  = 0, 5000
 	startT, stopT  = 0, Nbig//2
+	start, stop = Nbig//2, Nbig//2 + 20
+	startB, stopB = Nbig//2 + 1 , Nbig//2 + 21
+	delta = 0.420374134464041
+
 	
 
 	fitsliceT = slice(startT+4500, startT + 4600)
@@ -280,54 +267,62 @@ for i, beta in enumerate(betalist):
 	axSL[0,0].semilogy(tau[llplotslice]/beta, np.abs(np.real(GDtau[llplotslice])),c = col, label = lab)
 	axSL[0,0].semilogy(tau[llplotslice]/beta, np.abs(np.real(Gtau[llplotslice])),c = col, ls='--')
 	# axSL[0,0].semilogy(tau[startT:stopT]/beta, np.exp(mT*tau[startT:stopT] + cT), label=f'Fit with slope {mT:.03f}')
-	axSL[0,0].legend(framealpha=0)
+	# axSL[0,0].legend(framealpha=0)
 
 
 
 	axSL[0,1].semilogy(tau[startT:stopT]/beta, np.abs(np.real(GODtau[startT:stopT])),c = col, label = lab)
-	axSL[0,1].legend(framealpha=0)
+	# axSL[0,1].legend(framealpha=0)
 
 	axSL[1,0].semilogy(tau[startT:stopT]/beta, np.abs(np.real(DDtau[startT:stopT])),c=col,label=lab)
 	axSL[1,0].semilogy(tau[startT:stopT]/beta, np.abs(np.real(Dtau[startT:stopT])),c=col,ls='--')
-	axSL[1,0].legend(framealpha=0)
+	# axSL[1,0].legend(framealpha=0)
 
 	axSL[1,1].semilogy(tau[startT:stopT]/beta, np.abs(np.real(DODtau[startT:stopT])),c=col,label=lab)
-	axSL[1,1].legend(framealpha=0)
-	axSL[1,1].tick_params(axis='both', labelsize=8,pad=0)
+	# axSL[1,1].legend(framealpha=0)
+	# axSL[1,1].tick_params(axis='both', labelsize=4,pad=0)
 
 
-	GDinsetax.plot(tau/beta, np.real(GDtau), c=col, ls = '-',linewidth=0.8)
-	GDinsetax.plot(tau/beta, np.real(Gtau), c=col, ls='--',linewidth=0.8)
-	GDinsetax.set_xlabel(r'$\tau/\beta$',labelpad = 0, fontsize=8)
-	GDinsetax.tick_params(axis='both', labelsize=4,pad=0.5)
-	# GDinsetax.set_ylabel(r'$|\Re{G_{d}(\tau)}|$')
-	# GDinsetax.legend()
+	# GDinsetax.tick_params(axis='both', labelsize=6,pad=0.5)
+	# GODinsetax.tick_params(axis='both', labelsize=6,pad=0.5)
+	# DDinsetax.tick_params(axis='both', labelsize=6,pad=0.5)
+	# DODinsetax.tick_params(axis='both', labelsize=6,pad=0.5)
 
-	GODinsetax.plot(tau/beta, np.real(GODtau), c = col, ls='-', linewidth=0.8)
-	GODinsetax.plot(tau/beta, np.zeros_like(GODtau), c= col, ls = '--', linewidth=0.8)
-	GODinsetax.set_xlabel(r'$\tau/\beta$',labelpad = 0, fontsize=8)
-	GODinsetax.tick_params(axis='both', labelsize=4,pad=0.5)
-	# GODinsetax.set_ylabel(r'$|\Re{G_{od}(\tau)}$|')
+	GDinsetax.loglog(omega[start:stop]/(g**2), -np.imag(GDomega[start:stop])*(g**2),'.',c=col, label = lab)
+	# GDinsetax.loglog(omega[start:stop]/(g**2), conf_fit_GD[start:stop],'k--',label = 'ES power law')
+	GDinsetax.loglog(omega[start:stop]/(g**2), -np.imag(Gomega[start:stop])*(g**2),':',c='b',linewidth=0.8)
+
+	# GDinsetax.loglog(omega[start:stop]/(g**2), np.exp(c)*np.abs(omega[start:stop]/(g**2))**m, label=f'Fit with slope {m:.03f}')
+
+
+	# axLL[0,1].loglog(omega[start:stop]/(g**2), np.imag(GODomega[start:stop])*(g**2),'p',label = 'numerics Im GODomega')
+	GODinsetax.loglog(omega[start:stop]/(g**2), np.abs(GODomega[start:stop])*(g**2),'.',c=col,label = lab)
 	# GODinsetax.legend()
 
-	DDinsetax.plot(tau/beta, np.real(DDtau), c = col, ls='-', linewidth=0.8)
-	DDinsetax.plot(tau/beta, np.real(Dtau), c = col, ls= '--', linewidth=0.8)
-	DDinsetax.set_xlabel(r'$\tau/\beta$',labelpad = 0, fontsize=8)
-	DDinsetax.tick_params(axis='both', labelsize=4,pad=0.5)
-	# DDinsetax.set_ylabel(r'$|\Re{D_{d}(\tau)}|$')
+	DDinsetax.loglog(nu[startB:stopB]/(g**2), np.real(DDomega[startB:stopB])*(g**2),'.',c=col,label=lab)
+	DDinsetax.loglog(nu[startB:stopB]/(g**2), np.real(Domega[startB:stopB])*(g**2),':',c='b',linewidth=0.8)
+	# DDinsetax.loglog(nu[startB:stopB]/(g**2), conf_fit_DD,'k--',label = 'ES power law')
 	# DDinsetax.legend()
 
-	DODinsetax.plot(tau/beta, np.real(DODtau), c = col, ls='-', linewidth=0.8)
-	DODinsetax.plot(tau/beta, np.zeros_like(DODtau), c = col, ls='--', linewidth=0.8)
-	DODinsetax.set_xlabel(r'$\tau/\beta$',labelpad = 0, fontsize=8)
-	DODinsetax.tick_params(axis='both', labelsize=4,pad=0.5)
-	# DODinsetax.set_ylabel(r'$|\Re{D_{od}(\tau)}|$')
+
+	DODinsetax.loglog(nu[startB:stopB]/(g**2), -np.real(DODomega[startB:stopB])*(g**2),'.',c=col,label=lab)
+	# DODinsetax.legend()
+
+	
 	# DODinsetax.legend()
 
 # fig.savefig('GreenFunctionPlotsMetal.pdf', bbox_inches='tight')
 
-# figSL.savefig('INSETGreenFunctionPlotsMetal.pdf', bbox_inches='tight')
+axSL[1,1].yaxis.set_minor_formatter(NullFormatter())
+# axSL[1,1].ticklabel_format(axis='y', scilimits=(0,0))
+# axSL[1,1].tick_params('y',labelsize=2)
+handles, labels = axSL[0,0].get_legend_handles_labels()
+lgd = figSL.legend(handles, labels, ncol=len(labels)//2+1, loc="lower center", bbox_to_anchor=(1,-0.35),frameon=True,fancybox=True,borderaxespad=0, bbox_transform=axSL[1,0].transAxes)
+for insetax in insetaxes:
+	insetax.set_xticks([],labels=[])
+	insetax.set_yticks([],labels=[])
 
+figSL.savefig('LLINSETGreenFunctionPlotsMetal.pdf', bbox_inches='tight')
 # plt.savefig('GreenFunctionPlotsMetal.pdf', bbox_inches='tight')
 
 #plt.savefig('../../KoenraadEmails/lowenergy_powerlaw_ImagTime_SingleYSYK.pdf', bbox_inches = 'tight')
